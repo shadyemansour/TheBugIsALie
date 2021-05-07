854b6d38202712315758d20de98a2f9d4babde8f
Mansour <mansour@cip.ifi.lmu.de>
2021-05-07 13:01:43 +0200

    Created games table in db

--- de.lmu.ifi.sosy.tbial/etc/database/sql/create_tables_derby.sql
+++ de.lmu.ifi.sosy.tbial/etc/database/sql/create_tables_derby.sql
@@ -1,6 +1,15 @@
-drop table users ;
+drop table games;
+drop table users;
 
 create table users (
 	id int generated always as identity,
 	name varchar(255) unique not null,
 	password varchar(255) not null);
+
+create table games (
+                       id int generated always as identity,
+                       name varchar(255) unique not null,
+                       host varchar(255) references users(name),
+                       password varchar(255),
+                       gamestate varchar(255),
+                       numplayers int not null);

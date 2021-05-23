fb027c742a76ea58c4b2e78fa56f8f9835e14c71
Mansour <mansour@cip.ifi.lmu.de>
2021-05-23 14:23:44 +0200

    Sockets trial

--- de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/networking/Updater.java
+++ de.lmu.ifi.sosy.tbial/src/main/java/de/lmu/ifi/sosy/tbial/networking/Updater.java
@@ -7,7 +7,6 @@ import org.apache.wicket.protocol.ws.api.message.ConnectedMessage;
 import org.apache.wicket.protocol.ws.api.registry.IKey;
 import org.apache.wicket.protocol.ws.api.registry.IWebSocketConnectionRegistry;
 
-import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -15,62 +14,27 @@ import java.util.concurrent.TimeUnit;
 public class Updater {
     public static void start(ConnectedMessage message) {
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
-
-        Record[] data = generateData();
-
-        // create an asynchronous task that will write the data to the client
-        UpdateTask updateTask = new UpdateTask(message.getApplication(), message.getSessionId(), message.getKey(), data);
+        Message msg = new Message();
+        UpdateTask updateTask = new UpdateTask(message.getApplication(), message.getSessionId(), message.getKey(), msg);
         scheduledExecutorService.schedule(updateTask, 1, TimeUnit.SECONDS);
     }
 
 
 
 
-    /**
-     * Generates some random data to send to the client
-     * @return records with random data
-     */
-    private static Record[] generateData()
-    {
-        Random randomGenerator = new Random();
-        Record[] data = new Record[1000];
-        for (int i = 0; i < 1000; i++)
-        {
-            Record r = new Record();
-            r.year = 2000 + i;
-            r.field = (i % 2 == 0) ? "Company 1" : "Company 2";
-            r.value = randomGenerator.nextInt(1500);
-            data[i] = r;
-        }
-        return data;
-    }
-
-
-    /**
-     * A task that sends data to the client by pushing it to the web socket connection
-     */
-    private static class UpdateTask implements Runnable
-    {
-        private static final String JSON_SKELETON = "{ \"year\": \"%s\", \"field\": \"%s\", \"value\": %s }";
-
-        /**
-         * The following fields are needed to be able to lookup the IWebSocketConnection from
-         * IWebSocketConnectionRegistry
-         */
+    private static class UpdateTask implements Runnable {
         private final String applicationName;
         private final String sessionId;
         private final IKey key;
 
-        /**
-         * The data that has to be sent to the client
-         */
-        private final Record[] data;
 
-        private UpdateTask(Application application, String sessionId, IKey key, Record[] data){
+        private final Message msg;
+
+        private UpdateTask(Application application, String sessionId, IKey key, Message msg){
             this.applicationName = application.getName();
             this.sessionId = sessionId;
             this.key = key;
-            this.data = data;
+            this.msg = msg;
         }
 
         @Override
@@ -80,24 +44,16 @@ public class Updater {
             WebSocketSettings webSocketSettings = WebSocketSettings.Holder.get(application);
             IWebSocketConnectionRegistry webSocketConnectionRegistry = webSocketSettings.getConnectionRegistry();
 
-            int dataIndex = 0;
-
-            while (dataIndex < data.length)
-            {
+            while (true){//todo
                 IWebSocketConnection connection = webSocketConnectionRegistry.getConnection(application, sessionId, key);
-                try
-                {
-                    Record record = data[dataIndex++];
-                    String json = String.format(JSON_SKELETON, record.year, record.field, record.value);
+                try{
+                    String json = "";
 
-                    if (connection == null || !connection.isOpen())
-                    {
-                        // stop if the web socket connection is closed
+                    if (connection == null || !connection.isOpen()) {
                         return;
                     }
                     connection.sendMessage(json);
 
-                    // sleep for a while to simulate work
                     TimeUnit.SECONDS.sleep(1);
                 }
                 catch (InterruptedException x)
@@ -114,13 +70,9 @@ public class Updater {
         }
     }
 
-    /**
-     * The data that is being sent to the client in JSON format
-     */
-    private static class Record
+    private static class Message
     {
-        private int year;
-        private String field;
-        private int value;
+        private String type;
+        private String msg;
     }
 }
